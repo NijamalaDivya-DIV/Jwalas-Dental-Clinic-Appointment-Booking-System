c9267fc4888479ed23d1401a17d42cf1575bc354
NijamalaDivya-DIV <divyanijamala@gmail.com>
2026-04-17 12:06:55 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+appointments = []
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/booking', methods=['GET', 'POST'])
+def booking():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        date = request.form['date']
+
+        appointments.append({
+            "name": name,
+            "phone": phone,
+            "date": date
+        })
+
+        return render_template('success.html', name=name)
+
+    return render_template('booking.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
